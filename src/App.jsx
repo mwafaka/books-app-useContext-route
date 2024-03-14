a8515ed7ce9ef3541d6eb511dd661b73cf46a100
mwafaka <mwafaqarbash3@gmail.com>
2024-03-14 14:55:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import  {BooksProvider}  from './BooksContext'
+import './App.css'
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import BookToRead from './pages/BookToRead'
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+       <BooksProvider>
+       <Navbar/>
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/toread' element={<BookToRead/>}/>
+       </Routes>
+     </BooksProvider>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
